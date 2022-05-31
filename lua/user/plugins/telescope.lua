04dbad6a10e40bce267782219c9bf589a49d82db
Edgar Avila <avila.agramonte.edg@gmail.com>
2022-05-31 15:57:42 -0500

    Added telescope lsp and git mappings

--- lua/user/plugins/telescope.lua
+++ lua/user/plugins/telescope.lua
@@ -17,3 +17,15 @@ vim.api.nvim_set_keymap("n", "<leader>ff", "<cmd>Telescope find_files<cr>", {nor
 vim.api.nvim_set_keymap("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", {noremap = true})
 vim.api.nvim_set_keymap("n", "<leader>fb", "<cmd>Telescope buffers<cr>", {noremap = true})
 vim.api.nvim_set_keymap("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", {noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>fc", "<cmd>Telescope colorscheme<cr>", {noremap = true})
+
+-- Lsp mappings
+vim.api.nvim_set_keymap("n", "<leader>fw", "<cmd>Telescope lsp_references<cr>", {noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>fs", "<cmd>Telescope lsp_document_symbols<cr>", {noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>fS", "<cmd>Telescope lsp_workspace_symbols<cr>", {noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>fd", "<cmd>Telescope diagnostics<cr>", {noremap = true})
+
+-- Git mappings
+vim.api.nvim_set_keymap("n", "<leader>gc", "<cmd>Telescope git_commits<cr>", {noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>gs", "<cmd>Telescope git_status<cr>", {noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>gb", "<cmd>Telescope git_branches<cr>", {noremap = true})
