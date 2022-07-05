b5cf3712aef6caa3e2b288f89ccb3e669cb5e983
Edgar Avila <avila.agramonte.edg@gmail.com>
2022-07-05 17:40:26 -0500

    Added mappings to make CWD init.lua or current file

--- lua/user/mappings.lua
+++ lua/user/mappings.lua
@@ -4,6 +4,12 @@ vim.api.nvim_set_keymap("i", "jj", "<Esc>", {noremap = true})
 -- Better window navigation
 vim.api.nvim_set_keymap("n", "<leader>w", "<C-w>", {noremap = true})
 
+-- Fast edit init.lua "cd init.lua"
+vim.api.nvim_set_keymap("n", "<leader>cdi", ":e $MYVIMRC<CR>:cd %:p:h<CR>", {noremap = true})
+
+-- Change current working directory "cd file"
+vim.api.nvim_set_keymap("n", "<leader>cdf", ":cd %:p:h<CR>", {noremap = true})
+
 -- Closing brackets and quotes
 vim.api.nvim_set_keymap("i", "'", "''<left>", {noremap = true})
 vim.api.nvim_set_keymap("i", "\"", "\"\"<left>", {noremap = true})
