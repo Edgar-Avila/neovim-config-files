b3b07f71ac860944e06cc233eb113890f64f92d7
Edgar <avila.agramonte.edg@gmail.com>
2022-12-10 17:31:36 -0500

    More commands to run code

--- lua/user/commands.lua
+++ lua/user/commands.lua
@@ -1,6 +1,6 @@
 -- Run Python code
-vim.api.nvim_command("autocmd filetype python map <buffer> <f5> :w<cr>:exec \"!python3\" shellescape(@%, 1)<cr>")
-vim.api.nvim_command("autocmd filetype python imap <buffer> <f5> <esc>:w<cr>:exec \"!python3\" shellescape(@%, 1)<cr>")
+vim.api.nvim_command("autocmd filetype python map <buffer> <f5> :w<cr>:exec \"!python\" shellescape(@%, 1)<cr>")
+vim.api.nvim_command("autocmd filetype python imap <buffer> <f5> <esc>:w<cr>:exec \"!python\" shellescape(@%, 1)<cr>")
 
 -- run Go code
 vim.api.nvim_command("autocmd filetype go map <buffer> <f5> :w<cr>:exec \"!go run .\"<CR>")
@@ -9,6 +9,14 @@ vim.api.nvim_command("autocmd fileType go imap <buffer> <F5> <esc>:w<cr>:exec \"
 -- run Rust code
 vim.api.nvim_command("autocmd filetype rust map <buffer> <f5> :w<cr>:exec \"!cargo run\"<CR>")
 vim.api.nvim_command("autocmd fileType rust imap <buffer> <F5> <esc>:w<cr>:exec \"!cargo run\"<CR>")
+
+-- run cpp code
+vim.api.nvim_command("autocmd filetype cpp map <buffer> <f5> :w<cr>:exec \"!cmake .\"<CR>")
+vim.api.nvim_command("autocmd fileType cpp imap <buffer> <F5> <esc>:w<cr>:exec \"!cmake .\"<CR>")
+
+-- run js code
+vim.api.nvim_command("autocmd filetype javascript map <buffer> <f5> :w<cr>:exec \"!node .\"<CR>")
+vim.api.nvim_command("autocmd fileType javascript imap <buffer> <F5> <esc>:w<cr>:exec \"!node .\"<CR>")
 
 -- Set background transparent
 -- vim.cmd [[
