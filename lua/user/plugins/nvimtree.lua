dcb316a5f0f9ea80aac5acaedc9bed5f75e315bd
Edgar <avila.agramonte.edg@gmail.com>
2022-12-16 11:31:48 -0500

    Keep directories open on nvim tree toggle

--- lua/user/plugins/nvimtree.lua
+++ lua/user/plugins/nvimtree.lua
@@ -1,5 +1,5 @@
 require('nvim-tree').setup()
 
 -- Mappings
-vim.api.nvim_set_keymap("n", "<leader>t", ":NvimTreeToggle .<CR>", {noremap=true})
+vim.api.nvim_set_keymap("n", "<leader>t", ":NvimTreeToggle<CR>", {noremap=true})
 vim.api.nvim_set_keymap("n", "<leader>n", ":NvimTreeFocus<CR>", {noremap=true})
