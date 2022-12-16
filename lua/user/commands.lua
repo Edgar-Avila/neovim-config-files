0eeeda0de08aeab050cb119607e3ea26184f6ee4
Edgar <avila.agramonte.edg@gmail.com>
2022-12-16 11:27:49 -0500

    Fix js run command and add ts run command

--- lua/user/commands.lua
+++ lua/user/commands.lua
@@ -2,21 +2,25 @@
 vim.api.nvim_command("autocmd filetype python map <buffer> <f5> :w<cr>:exec \"!python\" shellescape(@%, 1)<cr>")
 vim.api.nvim_command("autocmd filetype python imap <buffer> <f5> <esc>:w<cr>:exec \"!python\" shellescape(@%, 1)<cr>")
 
--- run Go code
+-- Run Go code
 vim.api.nvim_command("autocmd filetype go map <buffer> <f5> :w<cr>:exec \"!go run .\"<CR>")
 vim.api.nvim_command("autocmd fileType go imap <buffer> <F5> <esc>:w<cr>:exec \"go run .\"<CR>")
 
--- run Rust code
+-- Run Rust code
 vim.api.nvim_command("autocmd filetype rust map <buffer> <f5> :w<cr>:exec \"!cargo run\"<CR>")
 vim.api.nvim_command("autocmd fileType rust imap <buffer> <F5> <esc>:w<cr>:exec \"!cargo run\"<CR>")
 
--- run cpp code
+-- Run cpp code
 vim.api.nvim_command("autocmd filetype cpp map <buffer> <f5> :w<cr>:exec \"!cmake .\"<CR>")
 vim.api.nvim_command("autocmd fileType cpp imap <buffer> <F5> <esc>:w<cr>:exec \"!cmake .\"<CR>")
 
--- run js code
-vim.api.nvim_command("autocmd filetype javascript map <buffer> <f5> :w<cr>:exec \"!node .\"<CR>")
-vim.api.nvim_command("autocmd fileType javascript imap <buffer> <F5> <esc>:w<cr>:exec \"!node .\"<CR>")
+-- Run js code
+vim.api.nvim_command("autocmd filetype javascript map <buffer> <f5> :w<cr>:!node \"%\"<CR>")
+vim.api.nvim_command("autocmd fileType javascript imap <buffer> <F5> <esc>:w<cr>:!node \"%\"<CR>")
+
+-- Run ts code
+vim.api.nvim_command("autocmd filetype typescript map <buffer> <f5> :w<cr>:!ts-node \"%\"<CR>")
+vim.api.nvim_command("autocmd fileType typescript imap <buffer> <F5> <esc>:w<cr>:!ts-node \"%\"<CR>")
 
 -- Set background transparent
 -- vim.cmd [[
