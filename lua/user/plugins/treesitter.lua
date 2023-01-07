f8eb4c1a2ef7ca2fb41b2a735a64819082b48e88
Edgar <avila.agramonte.edg@gmail.com>
2023-01-06 20:04:26 -0500

    Install vim treesitter parser due to errors

--- lua/user/plugins/treesitter.lua
+++ lua/user/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "python", "cpp", "typescript", "javascript", "lua", "go", "rust"},
+  ensure_installed = { "python", "cpp", "typescript", "javascript", "go", "rust", "lua", "vim"},
   sync_install = false,
   highlight = {
     enable = true,
