698b79917f78168f6e2ad1968d8959b173c9780b
Edgar-Avila <avila.agramonte.edg@gmail.com>
2023-12-30 14:31:48 -0500

    Zen mode tweaks

--- lua/user/plugins/zen-mode.lua
+++ lua/user/plugins/zen-mode.lua
@@ -1,5 +1,16 @@
 return {
     "folke/zen-mode.nvim",
+    opts = {
+        window = {
+            options = {
+                signcolumn = "no",
+                colorcolumn = "0",
+            }
+        },
+    },
+    init = function()
+        vim.cmd [[highlight ZenBg guibg=#000000 ctermbg=black]]
+    end,
     keys = {
         { "<leader>z", "<cmd>ZenMode<CR>" },
     },
