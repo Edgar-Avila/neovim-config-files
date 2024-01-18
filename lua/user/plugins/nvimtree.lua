990307ffcbfec262919b7a494a9d1e16aab19de6
Edgar-Avila <avila.agramonte.edg@gmail.com>
2024-01-17 19:00:19 -0500

    Auto close nvim-tree if it is the last window

--- lua/user/plugins/nvimtree.lua
+++ lua/user/plugins/nvimtree.lua
@@ -8,4 +8,10 @@ return {
     },
     main = 'nvim-tree',
     config = true,
+    init = function()
+        vim.api.nvim_create_autocmd('BufEnter', {
+            command = "if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif",
+            nested = true,
+        })
+    end,
 }
