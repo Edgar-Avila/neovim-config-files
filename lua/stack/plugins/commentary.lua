ac1ab4f5bf8821e6a71002b2a03aabebe33cea26
Edgar-Avila <avila.agramonte.edg@gmail.com>
2024-02-14 21:21:44 -0500

    Comment nvim plugin

--- lua/stack/plugins/commentary.lua
+++ lua/stack/plugins/commentary.lua
@@ -1,8 +1,12 @@
 return {
-    'tpope/vim-commentary',
-    keys = {
-        { "gcc", desc = "Comment current line" },
-        { "gc",  mode = "v",                   desc = "Comment selected text" }
-    },
-    lazy = true,
+    'numToStr/Comment.nvim',
+    event = "BufReadPre",
+    config = function()
+        require('Comment').setup()
+        local ft = require('Comment.ft')
+        -- ft.javascriptreact = { '{/*%s*/}' }
+        -- ft.typescriptreact = { '{/*%s*/}' }
+        ft.angular = ft.get('html')
+        ft.heex = ft.get('html')
+    end,
 }
