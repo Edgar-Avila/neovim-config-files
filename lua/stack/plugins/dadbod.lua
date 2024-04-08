eb4b263ab7cfb9a2982002ab9be21ecd7704f35a
Edgar-Avila <avila.agramonte.edg@gmail.com>
2024-04-07 19:27:01 -0500

    feat: Database management

---
+++ lua/stack/plugins/dadbod.lua
@@ -0,0 +1,21 @@
+return {
+    'kristijanhusak/vim-dadbod-ui',
+    dependencies = {
+        { 'tpope/vim-dadbod',                     lazy = true },
+        { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+    },
+    cmd = {
+        'DBUI',
+        'DBUIToggle',
+        'DBUIAddConnection',
+        'DBUIFindBuffer',
+    },
+    keys = {
+        { '<leader>db', ':DBUIToggle<CR>' },
+    },
+    init = function()
+        vim.api.nvim_command(
+        "autocmd FileType sql,mysql,plsql lua require('cmp').setup.buffer({ sources = {{ name = 'vim-dadbod-completion' }} })")
+        vim.g.db_ui_use_nerd_fonts = 1
+    end
+}
